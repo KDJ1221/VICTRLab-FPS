8b7422bfc4a9c00dc698dd6138d4fbd3c48e9709
Kimmiko James <kimmikojames@gmail.com>
2017-12-17 03:35:21 -0800

    Attempted to add muzzle flash
    Needs fixing

--- VICTRLab FPS/Assets/GunStuff/GunFire.cs
+++ VICTRLab FPS/Assets/GunStuff/GunFire.cs
@@ -7,6 +7,7 @@ public class GunFire : MonoBehaviour {
     public float range = 100f;
 
     public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
     
 	// Update is called once per frame
 	void Update () {
@@ -20,6 +21,8 @@ public class GunFire : MonoBehaviour {
 
         }
         public void Shoot(){
+
+            muzzleFlash.Play();
 
             RaycastHit hit;
             if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) {
