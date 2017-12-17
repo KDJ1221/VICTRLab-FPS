10b724805e551705f120b8838732b8226b93538d
Kimmiko James <kimmikojames@gmail.com>
2017-12-17 03:02:46 -0800

    Added Crosshair

--- VICTRLab FPS/Assets/GunStuff/GunFire.cs
+++ VICTRLab FPS/Assets/GunStuff/GunFire.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class GunFire : MonoBehaviour {
+
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
     
 	// Update is called once per frame
 	void Update () {
@@ -10,6 +14,16 @@ public class GunFire : MonoBehaviour {
             AudioSource gunsound = GetComponent<AudioSource>();
             gunsound.Play();
             GetComponent<Animation>().Play("GunShot");
+
+            Shoot();
+        }
+
+        }
+        public void Shoot(){
+
+            RaycastHit hit;
+            if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) {
+                Debug.Log(hit.transform.name);
+            }
         }
 	}
-}
