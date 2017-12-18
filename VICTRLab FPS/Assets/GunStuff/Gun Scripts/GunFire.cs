ffb131f47f443ca894ca781e723ff28508dc9d94
Kimmiko James <kimmikojames@gmail.com>
2017-12-17 17:43:19 -0800

    Gun is mostly done

--- VICTRLab FPS/Assets/GunStuff/Gun Scripts/GunFire.cs
+++ VICTRLab FPS/Assets/GunStuff/Gun Scripts/GunFire.cs
@@ -5,20 +5,27 @@ public class GunFire : MonoBehaviour {
 
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 30f;
 
     public Camera fpsCam;
     //public ParticleSystem muzzleFlash;
 
     public GameObject muzzle1;
     public GameObject muzzle2;
+
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
     
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("Fire1")) {
+        if (Input.GetButtonDown("Fire1")&& Time.time >= nextTimeToFire) {
             AudioSource gunsound = GetComponent<AudioSource>();
             gunsound.Play();
             GetComponent<Animation>().Play("GunShot");
 
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
 
@@ -35,7 +42,14 @@ public class GunFire : MonoBehaviour {
                 target.TakeDamage(damage);
             }
 
+            if(hit.rigidbody != null) {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
             }
+
+            }
+
+        GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impactGO, 2f);
         muzzle1.SetActive(true);
         muzzle2.SetActive(true);
 
