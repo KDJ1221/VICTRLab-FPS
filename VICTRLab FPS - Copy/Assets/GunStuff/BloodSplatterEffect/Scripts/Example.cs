dbd7cc9f50271a51f21ce6f6dacdef611172788b
Kimmiko James <kimmikojames@gmail.com>
2018-02-06 10:50:32 -0800

    ignore --> deleted screenshot crap

---
+++ VICTRLab FPS - Copy/Assets/GunStuff/BloodSplatterEffect/Scripts/Example.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour {
+
+    public GameObject bloodSplatter1;
+    public GameObject bloodSplatter2;
+    public GameObject bloodSplatter3;
+    public GameObject bloodSplatter4;
+    
+    //UGUI button functions
+    
+    public void Blood1(){
+    Instantiate(bloodSplatter1);
+    }
+    
+    public void Blood2(){
+    Instantiate(bloodSplatter2);
+    }
+    
+    public void Blood3(){
+    Instantiate(bloodSplatter3);
+    }
+    
+    public void Blood4(){
+    Instantiate(bloodSplatter4);
+    }
+    
+    void Update(){
+    //Swing the camera for Demo scene
+    this.transform.Rotate(new Vector3(0,0.25f,0) * Mathf.Sin(Time.time)); 
+    }
+}
---
+++ VICTRLab FPS - Copy/Assets/Scripts/LoadLevel1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel1 : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col) {
+        if(col.gameObject.tag == "Player") {
+            SceneManager.LoadScene("Level1");
+        }
+    }
+}
