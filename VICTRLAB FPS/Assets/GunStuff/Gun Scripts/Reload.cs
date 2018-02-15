e6a8c62b9ca0408b3720d33bc47b3161109355a9
jeffnwarner <jnwarner@ucdavis.edu>
2018-02-15 12:43:39 -0800

    Fully implemented reload functionality

---
+++ VICTRLAB FPS/Assets/GunStuff/Gun Scripts/Reload.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reload : MonoBehaviour {
+
+    public AudioSource ReloadSound;
+    //public GameObject CrossObject;
+    //public GameObject MechanicsObject;
+    public int ClipCount;
+    public int ReserveCount;
+    public int ReloadAvailable;
+    public static GunFire GunComponent;
+    public static AimDownSights aimScript;
+
+	void Start () {
+        GunComponent = GetComponent<GunFire>();
+#pragma warning disable CS0436 // Type conflicts with imported type
+        aimScript = GetComponent<AimDownSights>();
+#pragma warning restore CS0436 // Type conflicts with imported type
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ClipCount = GlobalAmmo.LoadedAmmo;
+        ReserveCount = GlobalAmmo.CurrentAmmo;
+        if(ReserveCount == 0) {
+            ReloadAvailable = 0;
+        }
+        else {
+            ReloadAvailable = 10 - ClipCount;
+        }
+
+        if(Input.GetButtonDown("Reload")) {
+            if(ReloadAvailable >= 1) {
+                if(ReserveCount <= ReloadAvailable) {
+                    GlobalAmmo.LoadedAmmo += ReserveCount;
+                    GlobalAmmo.CurrentAmmo -= ReserveCount;
+                }
+                else {
+                    GlobalAmmo.LoadedAmmo += ReloadAvailable;
+                    GlobalAmmo.CurrentAmmo -= ReloadAvailable;
+                }
+                ActionReload();
+            }
+            StartCoroutine(EnableScripts());
+        }
+	}
+
+    IEnumerator EnableScripts() {
+        yield return new WaitForSeconds(1.1f);
+        //aimScript.StopAim();
+        aimScript.enabled = true;
+        GunComponent.enabled = true;
+        //CrossObject.SetActive(true);
+        //MechanicsObject.SetActive(true);
+    }
+
+    void ActionReload() {
+        GunComponent.enabled = false;
+        aimScript.StopAim();
+        aimScript.enabled = false;
+        //CrossObject.SetActive(false);
+        //MechanicsObject.SetActive(false);
+        ReloadSound.Play();
+        GetComponent<Animation>().Play("Reload");
+    }
+}
