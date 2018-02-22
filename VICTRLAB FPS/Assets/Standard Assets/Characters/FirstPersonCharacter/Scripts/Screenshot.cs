753e775d330fa3dfadb5968f2e617328e7220bdd
jeffnwarner <jnwarner@ucdavis.edu>
2018-02-22 14:37:35 -0800

    Revert "Got rid of the stupid screenshot stuff"
    This reverts commit 1455a8c2e49e1c141a99228fe1414a59b495ee3b.

---
+++ VICTRLAB FPS/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Screenshot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour {
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.K))
+        {
+            ScreenCapture.CaptureScreenshot(("C:\\Users\\Jeffrey\\Documents\\GitHub\\VICTRLab-FPS\\VICTRLAB FPS\\Screenshots" + System.DateTime.Now.ToString("__yyyy-MM-dd") + ".png"));
+        }
+	}
+}
