504366b82d25d6878317dd3fb1549d01ef80c7ed
Kimmiko James <kimmikojames@gmail.com>
2018-04-22 19:54:48 -0700

    Added First Camera View

---
+++ VICTRLAB FPS/Assets/Scripts/AnimationManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour {
+
+    public static AnimationManager instance = null;
+
+    [SerializeField] private List<GameObject> cameras = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		if (instance == null) {
+            instance = this;
+        } else if(instance != this) {
+            Destroy(gameObject);
+        }
+	}
+
+    void StartNextCamera() {
+        if(cameras.Count > 0) {
+            cameras[0].SetActive(true);
+        }
+    }
+	
+	public void DestroyCamera(GameObject camera) {
+        cameras.RemoveAt(0);
+        Destroy(camera);
+        StartNextCamera();
+    }
+}
