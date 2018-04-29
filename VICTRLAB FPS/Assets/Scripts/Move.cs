48753da5158fa581b31afe1cbbbfec80e2358d5a
Kimmiko James <kimmikojames@gmail.com>
2018-04-29 15:32:48 -0700

    Enemy waypoint

--- VICTRLAB FPS/Assets/Scripts/Move.cs
+++ VICTRLAB FPS/Assets/Scripts/Move.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Move : MonoBehaviour {
 
+    [SerializeField]
+    Transform destination;
+
+    NavMeshAgent _navMeshAgent;
+
 	// Use this for initialization
 	void Start () {
-		
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if (_navMeshAgent == null)
+        {
+            Debug.LogError("The nav mesh agent component isn't attached");
+        }
+        else
+        {
+            SetDestination();
+        }
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void SetDestination()
+    {
+        if(destination != null)
+        {
+            Vector3 targetVector = destination.transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+        }
+    }
 }
