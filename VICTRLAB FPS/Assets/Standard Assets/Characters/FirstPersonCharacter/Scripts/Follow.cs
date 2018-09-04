ecda1e1eb3566ddd153f67fd7c32a8a83762a846
Jeffrey Warner <jnwarner@ucdavis.edu>
2018-09-03 23:50:14 -0700

    fixed tilting when dead, and added exit sign

--- VICTRLAB FPS/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Follow.cs
+++ VICTRLAB FPS/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Follow.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 
-public class Follow : MonoBehaviour {
+public class Follow : MonoBehaviour
+{
     //remember to make variables for numbers so they aren't just hard coded 
     public Transform Player;
     public Vector3 newPosition;
@@ -16,6 +17,7 @@ public class Follow : MonoBehaviour {
     bool isSquat = false;
     bool reload = false;
     bool canShoot = false;
+    private static bool enemyOnStairs = false;
     public bool CharacterTypeSquat;
     public bool CharacterTypeReload;
     float t;
@@ -40,7 +42,6 @@ public class Follow : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-
         //Vector3 playerDir = Player.position - transform.position;
         //Debug.Log("playerDir is " + playerDir);
         //float angle = Vector3.Angle(playerDir, Player.transform.forward);
@@ -49,7 +50,6 @@ public class Follow : MonoBehaviour {
         //{
         //    anim.SetBool("isShooting", false);
         //    anim.SetBool("isIdle", false);
-        //    anim.SetBool("isWalking", false);
         //    anim.SetBool("isCrouch", true);
         //    isSquat = true;
         //    Debug.Log("I'm looking at the enemy so he should crouch");
@@ -62,15 +62,18 @@ public class Follow : MonoBehaviour {
         //Debug.Log("The distance is: " + distance);
         //Debug.Log(ammo);
 
-        if (check > 0.97f && targetScript.GetCount() > 3 && distance > 3) //or less than 3 players
-                                                                          // && !Physics.Linecast(forward, this.transform.position)
-        {
-            //Debug.Log("Facing the object" + check);
-            //ChangeState("Squat");
+        if (reload) {
+            reload = false;
+            //Debug.Log(ammo);
+            ChangeState("Reload");
+            ammo = 60;
+            //Debug.Log(ammo);
+        }
+
+        if (check > 0.97f && targetScript.GetCount() > 3 && distance > 2.0f && rayHit.transform.gameObject != playerTarget) {
             anim.SetBool("isCrouch", true);
             anim.SetBool("isShooting", false);
             anim.SetBool("isIdle", false);
-            anim.SetBool("isWalking", false);
             cc.height = 1f;
             isSquat = true;
         }
@@ -82,7 +85,6 @@ public class Follow : MonoBehaviour {
             anim.SetBool("isCrouch", false);
             //anim.SetBool("isShooting", true);
             anim.SetBool("isIdle", false);
-            anim.SetBool("isWalking", false);
             cc.height = 2f;
             isSquat = false;
             //Attack();
@@ -94,23 +96,23 @@ public class Follow : MonoBehaviour {
                 //    rdm = UnityEngine.Random.Range(10, 20);
                 //    t = Time.time + rdm;
                 //}
-
-                if (reload) {
-                    reload = false;
-                    //Debug.Log(ammo);
-                    ChangeState("Reload");
-                    ammo = 60;
-                    //Debug.Log(ammo);
+                if (Math.Abs(Player.position.y - this.transform.position.y) > 1 && !enemyOnStairs) {
+                    enemyOnStairs = true;
+                    damage += 0.5f;
+                }
+                else if (Math.Abs(Player.position.y - this.transform.position.y) <= 1 && enemyOnStairs) {
+                    enemyOnStairs = false;
+                    damage -= 0.5f;
                 }
 
                 if (Math.Abs(Player.position.x - this.transform.position.x) < 25) {
                     if (!isDead && !isSquat && !reload) {
                         direction = Player.position - this.transform.position;
-                        direction.y = 0;
+                        if (Math.Abs(Player.position.y - this.transform.position.y) <= 1)
+                            direction.y = 0;
                         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
                         anim.SetBool("isIdle", false);
                         anim.SetBool("isCrouch", false);
-                        anim.SetBool("isWalking", false);
 
                         if (direction.magnitude > 0) {
                             anim.SetBool("isShooting", true);
@@ -129,6 +131,10 @@ public class Follow : MonoBehaviour {
                 }
             }
         }
+        if (isDead) {
+            direction.y = 0;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+        }
     }
 
 
@@ -138,6 +144,7 @@ public class Follow : MonoBehaviour {
         anim.SetBool("isDead", false);
         anim.SetBool("isIdle", false);
         anim.SetBool("isShooting", false);
+        anim.SetBool("isReload", false);
         anim.SetBool("isDead", true);
         isDead = true;
         if (isSquat) {
@@ -151,8 +158,7 @@ public class Follow : MonoBehaviour {
         isSquat = false;
     }
 
-    public IEnumerator GunCoolDown()
-    {
+    public IEnumerator GunCoolDown() {
         yield return new WaitForSeconds(2.5f);
         canShoot = true;
     }
@@ -183,7 +189,6 @@ public class Follow : MonoBehaviour {
                 isSquat = true;
                 anim.SetBool("isShooting", false);
                 anim.SetBool("isIdle", false);
-                anim.SetBool("isWalking", false);
                 anim.SetBool("isCrouch", true);
             }
         }
@@ -196,7 +201,7 @@ public class Follow : MonoBehaviour {
     void Attack() {
         //Debug.Log(canShoot);
 
-        if(Physics.Raycast(transform.position, transform.forward, out rayHit, 100)) {
+        if (Physics.Raycast(transform.position, transform.forward, out rayHit, 100)) {
             //GameObject bloodEffect = (GameObject)Instantiate(Resources.Load("Blood Effect"));
             //Debug.Log(rayHit.transform.name);
             //rayHit.transform.SendMessage("DamagePlayer", damage, SendMessageOptions.DontRequireReceiver);
@@ -231,11 +236,10 @@ public class Follow : MonoBehaviour {
     public void ChangeDamage(string change) {
         if (change.Equals("raise")) {
             damage += 1.0f;
-            //Debug.Log(damage);
         }
         else if (change.Equals("reset")) {
             damage = 0.5f;
             //Debug.Log(damage);
         }
     }
-}  
+}
