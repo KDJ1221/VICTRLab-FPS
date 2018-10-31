7db934e875b43adaed02571b9bdd68f4df476d13
Jeffrey Warner <jnwarner@ucdavis.edu>
2018-10-30 22:42:07 -0700

    Changed the "congratulations" text reworked the enemy placeement and ammo placement

---
+++ VICTRLAB FPS/Assets/Scripts/Congratulations.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Congratulations : MonoBehaviour {
+
+    public Text congrats;
+	void Start () {
+		if (PersistentManagerScript.Instance.isCop) {
+            congrats.text = "Congratulations! \nYou escaped Nearstrom and have detained the robbers!";
+        }
+        else {
+            congrats.text = "Congratulations! \nYou have escaped from the police!";
+        }
+	}
+	
+}
